c6da1052114ec9a53dd0e4169c3a3368b1ce8796
krystianekb <krystian.brachmanski@gmail.com>
2019-05-06 11:11:20 +0200

    Additional duration measurement logic

--- test-file-sorting-algorithm/src/main/java/org/krystianekb/sorting/file/MainApp.java
+++ test-file-sorting-algorithm/src/main/java/org/krystianekb/sorting/file/MainApp.java
@@ -1,6 +1,7 @@
 package org.krystianekb.sorting.file;
 
 import com.google.code.externalsorting.ExternalSort;
+import org.apache.commons.lang3.time.StopWatch;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,8 +10,16 @@ public class MainApp {
 
     public static void main(String[] args) throws IOException {
 
+        StopWatch stopwatch = new StopWatch();
+        stopwatch.start();
+
         ExternalSort.sort(new File("test.txt"),
                 new File("sorted_test_java.txt"));
+
+        stopwatch.stop();
+        long timeTaken = stopwatch.getTime();
+        System.out.println(
+                String.format("Execution time : %.2f s", timeTaken / 1000.0));
 
     }
 
